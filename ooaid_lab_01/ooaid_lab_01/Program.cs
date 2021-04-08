276809a35d7109c5c40bc9426c356eda3e03a022
Filippchik <82146421+Filippchik@users.noreply.github.com>
2021-04-08 13:26:09 +0300

    Add project

---
+++ ooaid_lab_01/ooaid_lab_01/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ooaid_lab_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, я Филипчик!");
+            Console.Write("Реши пример 2+5=");
+        }
+    }
+}
